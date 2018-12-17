bc86d359ba8b5d3221baf52a7952dedaf9050229
FZhu <hzhu225@gmail.com>
2018-12-17 15:27:03 +1100

    Add Intent

--- app/src/main/java/com/cms/twoactivities/SecondActivity.java
+++ app/src/main/java/com/cms/twoactivities/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.cms.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity
 {
@@ -11,5 +13,9 @@ public class SecondActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 }
