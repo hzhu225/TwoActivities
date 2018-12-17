e12d7592069f9814b192d7106ee8d06682774f19
FZhu <hzhu225@gmail.com>
2018-12-17 12:51:36 +1100

    Create second activity

---
+++ app/src/main/java/com/cms/twoactivities/SecondAcivity.java
@@ -0,0 +1,15 @@
+package com.cms.twoactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondAcivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
